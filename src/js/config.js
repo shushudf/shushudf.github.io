ab7c0539d21a8d87b6378d565cba2938095e1af8
shushudf <7w7w7w7w@naver.com>
2024-11-11 17:09:49 +0900

    Add files via upload

---
+++ src/js/config.js
@@ -0,0 +1,11 @@
+// [주의] 붙여넣기 하는 과정에서 export 키워드를 삭제하지 않도록 주의하세요!
+export const firebaseConfig = {
+  apiKey: "AIzaSyC1DhaLxpb5MQwxpUIOMEqpQ5ZuN_5zkU0",
+  authDomain: "messenger-3eff3.firebaseapp.com",
+  databaseURL: "https://messenger-3eff3-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  projectId: "messenger-3eff3",
+  storageBucket: "messenger-3eff3.firebasestorage.app",
+  messagingSenderId: "499128617852",
+  appId: "1:499128617852:web:92aa900709a3b00ccf32f9",
+  measurementId: ""
+};
